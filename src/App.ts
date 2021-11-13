28822122769042980a3f42233ecae28dcd73e052
nik <nik@hal.com>
2021-11-13 10:22:42 +0200

    adding a new question

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,5 @@
 import { ApiRequestService } from "./request-service";
-
+import { v4 as uuidv4 } from "uuid";
 export interface SurveyResponse {
   success: boolean;
   data: Survey[];
@@ -8,7 +8,7 @@ export interface SurveyResponse {
 export interface Survey {
   prompt: string;
   answers: [string?];
-  id: number;
+  id: string;
 }
 
 export class App {
@@ -17,11 +17,46 @@ export class App {
   private survey: Survey[] | undefined = [];
   private appNode = document.getElementById("app") as HTMLElement;
 
-  _buildQuestions(
+  _checkNewQuestionStatus(
+    survey: Survey[] | undefined,
+    addQuestionNode: HTMLButtonElement
+  ) {
+    if (survey?.length == 10) {
+      addQuestionNode.disabled = true;
+    } else {
+      addQuestionNode.disabled = false;
+    }
+  }
+
+  _addNewQuestionClick = (questionContent: DocumentFragment): void => {
+    const addQuestionNode = document.querySelector(
+      ".question__body--add"
+    ) as HTMLButtonElement;
+
+    addQuestionNode?.addEventListener("click", () => {
+      this.appNode.appendChild(questionContent.cloneNode(true));
+      this.survey?.push({ prompt: "Type", answers: [], id: uuidv4() });
+      this._checkNewQuestionStatus(this.survey, addQuestionNode);
+      console.log(this.survey);
+    });
+  };
+
+  _addNewQuestion = (theNode: HTMLElement) => {
+    return {
+      theNode,
+    };
+  };
+
+  _addZeroes = (input: string) => {
+    input.length == 1 ? true : (input = `0${input}`);
+    return input;
+  };
+
+  _buildQuestions = (
     questionContent: DocumentFragment,
     answerContent: DocumentFragment,
     survey: Survey[]
-  ) {
+  ) => {
     survey.forEach((question, index) => {
       this.appNode.appendChild(questionContent.cloneNode(true));
       const currentNode = this.appNode.querySelectorAll(".question")[index];
@@ -47,20 +82,18 @@ export class App {
         )[q_Index];
 
         //@ts-expect-error depends on template classes
-        currentAnswerIndexNode.innerHTML = `${q_Index + 1}`;
+        currentAnswerIndexNode.innerHTML = `${this._addZeroes(q_Index + 1)}`;
         //@ts-expect-error depends on template classes
         currentAnswerTextNode.innerHTML = `${answer}`;
       });
     });
-  }
+  };
 
-  buildDOM = (survey: Survey[]) => {
-    const questionNode = document.getElementById(
-      "question"
-    ) as HTMLTemplateElement;
-    const questionContent = questionNode.content;
-    const answerNode = document.getElementById("answer") as HTMLTemplateElement;
-    const answerContent = answerNode.content;
+  buildDOM = (
+    questionContent: DocumentFragment,
+    answerContent: DocumentFragment,
+    survey: Survey[]
+  ) => {
     this._buildQuestions(questionContent, answerContent, survey);
   };
 
@@ -68,7 +101,19 @@ export class App {
     const response = await this.rs.getSurvey();
     if (response !== undefined) {
       this.survey = await response.data;
-      this.buildDOM(this.survey);
+      console.log(this.survey);
+
+      const questionNode = document.getElementById(
+        "question"
+      ) as HTMLTemplateElement;
+      const questionContent = questionNode.content;
+      const answerNode = document.getElementById(
+        "answer"
+      ) as HTMLTemplateElement;
+      const answerContent = answerNode.content;
+
+      this.buildDOM(questionContent, answerContent, this.survey);
+      this._addNewQuestionClick(questionContent);
     }
   };
 }
